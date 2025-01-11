bb824f764452665c9c804d726f3739dec75b5905
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-11 14:06:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treated as newer version of JavaScript
+
+// alert("hello") //  we are using node js , not browser
+
+
+let name = "Nikhil"
+let age = 18
+let isLoggedIn = true
+
+
+// number =>2 to power 53 tak hai shayad
+// bigint
+// string =>  try to use ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+
+
+console.log(typeof age); // gives the type of data Type
+
+// null is an object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //object
