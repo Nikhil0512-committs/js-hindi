d90699a0106b033699aad449b059e2cd82002128
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-11 15:10:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(null > 0) //false
+// console.log (null == 0) //false
+// console.log(null >=0) //true(due to error)
+//comparison converts null to a number , so that's why 3rd one is true
+
+// === , strict check it checks the value as well as dataType
+
+console.log("2" ==2) //true
+console.log("2"===2) //false
