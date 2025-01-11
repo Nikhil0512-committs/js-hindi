24c969851c01a7f0115e094da6ee958730466a3a
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-11 13:46:47 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword ="212121"
+accountCity = "Bengaluru"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
