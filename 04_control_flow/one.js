5c06d0a0e401a41f214f371470e3cc8d11140b88
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-26 12:47:05 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if statement
+
+if(true){ // agar true hai to iske andar ka code chalega
+
+}
+=(single equal to ) , it is for assignment operator
+== // just only for value comparison
+=== // for both value and dataType comparison(strict equal)
+
+const temperature = 42
+if(temperature < 50){
+    console.log("Temperature is less than 50 degrees")
+}
