73150d5dd4d01d3658b68c6416e7909cb0b24a95
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-26 10:36:51 +0000

    arrows

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -13,3 +13,31 @@ user.welcomeMessage() // it will print Nikhil, Welcome to the website
 user.username = "Sam"
 user.welcomeMessage() // Ab isme naam Sam aayega kyunki ab username sam hogaya hai , context is changed here
 console.log(this) // Gives an empty object
+
+
+function chai(){
+    console.log(this)
+}
+chai() // Kaafi values milti hai
+
+
+// Arrow Function
+const chai = () => {
+    let username = "Nikhil"
+    console.log(this)
+}
+chai() // empty
+
+const addTwo = (num1,num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3,4)) // 7
+
+// Implicit return Arrow Function
+const chai = (num1,num2) => num1+num2
+console.log(chai(3,4)) // 7
+// Another method
+const addTwo = (num1,num2) => (num1+num2)
+console.log(addTwo(3,4)) // 7 , An object can be returned in this manner only
+
+// curly brackets wale me return keyword likhna hi padega
