635930405db1bebbdaf6c4bb2ce9ae62b6549a81
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-26 09:59:40 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,36 @@ console.log(a)
 console.log(b)
 console.log(c)
 // That's why var is not used as it comes out of block scope as well
+
+
+
+function one() {
+    const username = "Nikhil"
+
+    function two(){
+        const website = "nikhil.com"
+        console.log(username)
+    }
+    // console.log(website) // Gives error
+
+    two()
+}
+one()
+
+
+
+// +++++++ Interesting ++++++++
+
+// Different ways of declaring a function
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+
+
+console.log(addTwo(7)) // Gives error as addTwo cannot be used before initialization
+const addTwo = function(num){
+    return num + 2
+}
