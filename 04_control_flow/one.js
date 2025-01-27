472f9d596c8a4de7fcce96f046fe11bb1b881567
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-27 05:26:59 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -11,3 +11,24 @@ const temperature = 42
 if(temperature < 50){
     console.log("Temperature is less than 50 degrees")
 }
+
+const balance = 1000
+if(balance<500){
+    console.log("Balance is less than 500")
+} else if(balance < 750){
+    console.log("Balance is less than 750")
+} else{
+    console.log("Balance is less than 1200")
+}
+
+const userLoggedIn = true
+const debitCard = true
+if(userLoggedIn && debitCard){
+    console.log("allow to buy courses")
+}
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("You can access it")
+} // Here we have used Or , IF ANyone is even correct , it will go into the loop
