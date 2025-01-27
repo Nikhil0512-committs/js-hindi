8b1e369fdd4b59f8946b2bae7306806d02b569c9
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-01-27 06:55:04 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+const month = 3
+
+switch(month){
+    case 1:
+    console.log("January")
+    break;
+    case 2:
+    console.log("February")
+    break;
+    case 3:
+    console.log("march")
+    break;
+    
+    default:
+        console.log("WRONG")
+        break;
+}
+
+
+const mahina = "April"
+
+switch(mahina){
+    case "jan":
+        console.log("January")
+        break;
+
+    default :
+        console.log("error")
+        break;
+}
