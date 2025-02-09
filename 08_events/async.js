c6e1bd52a2846b7abb01a28757a5b3508bc21438
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-02-09 19:08:27 +0000

    async

---
+++ 08_events/async.js
@@ -0,0 +1,7 @@
+// In general it's a synchronous language which executes one line of code at a time and it's also a single threaded language
+// each operation waits for the last one to complete before executing
+
+// Sync code is blocking code which executes line by line , whereas async code is non-blocking code which operates multiply
+// Both are good on their own
+
+// JS Engine is made of memory heap and call stack
